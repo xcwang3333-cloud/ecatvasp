78fd93825e80b992f8e48b8796aaf9f7c896c990
xiaochen wang <xcwang3333@gmail.com>
2026-09-07 08:09:21 +0800

    feat: export application service API

--- src/ecatvasp/api/__init__.py
+++ src/ecatvasp/api/__init__.py
@@ -1,1 +1,21 @@
-"""Api package boundary for ECatVASP."""
+"""Application-service package boundary for ECatVASP."""
+
+from ecatvasp.api.application import (
+    ApplicationInspectionResult,
+    ApplicationReportFormat,
+    ApplicationReportResult,
+    ApplicationServiceError,
+    ApplicationStructurePromotionResult,
+    ApplicationVaspAnalysisResult,
+    ProjectApplicationService,
+)
+
+__all__ = [
+    "ApplicationInspectionResult",
+    "ApplicationReportFormat",
+    "ApplicationReportResult",
+    "ApplicationServiceError",
+    "ApplicationStructurePromotionResult",
+    "ApplicationVaspAnalysisResult",
+    "ProjectApplicationService",
+]
